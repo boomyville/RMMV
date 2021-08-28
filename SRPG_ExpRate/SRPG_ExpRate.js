92d60f9d6d5c7b87950435a3a3635ced08f489de
boomyville <boomyville@gmail.com>
2021-08-28 18:07:12 +1000

    Update SRPG_ExpRate.js

--- SRPG_ExpRate/SRPG_ExpRate.js
+++ SRPG_ExpRate/SRPG_ExpRate.js
@@ -1,24 +1,30 @@
 //=============================================================================
-// SRPG_ExpRate.js
+// SRPG_ExpMod.js
 //=============================================================================
 /*:
-  * @plugindesc A plugin that modifies how exp is gained through battles 
+  * @plugindesc A plugin that modifies how exp is gained through battles. Compatible with new version of SRPG_AoEAnimation.js
  * @author Boomy 
  * 
- * @param Regular EXP Formula 
- * @desc Lunatic code formula that calculates how much exp the user gets from a regular hit on a foe  
- * Can use parameters such as actor and enemy 
- * @default  enemy.exp() * 0.1
+ * @param Average AoE Target EXP
+ * @type boolean
+ * @desc Split EXP equal to number of targets. Note that SRPG_AoEAnimation.js already splits exp when mulltiple actors are targeted.
+ * @default false
  * 
- * @param Kill EXP Formula 
- * @desc Lunatic code formula that calculates how much exp the user gets from a kill 
- * Can use parameters such as actor and enemy 
- * @default enemy.exp() * 0.4
+ * @param EXP multiplier
+ * @desc Lunatic code formula. Can use parameters such as user and targetsAverageLevel, targetsMaxLevel, targetsMinLevel 
+ * @default 1
  *  
- * @param Skill EXP Formula 
- * @desc Lunatic code formula that calculates how much exp the user gets from a skill not targetting a foe 
- * Can use parameters such as actor
- * @default (actor.nextLevelExp() - actor.currentLevelExp()) * 0.1
+  * @help
+  * Alters the BattleManager.makeRewards function to make some modifications to 
+  * how much EXP is gained in SRPG battle.
+  *
+  * Kill an enemy: Gain full enemy EXP x modifiers 
+  * Damage an enemy: Gain enemy EXP x modifiers x srpg_core.js plugin parameter
+  * Take damage from enemy: Gain enemy EXP x modifiers x srpg_core.js plugin parameter
+  * Use skill on non-enemy: Gain a percentage of actor exp 
+*
+*  Note that if you are using SRPG_AoEAnimation.js, exp is distributed amongst all units active in battle.  
+  * 
  */
 //===============================================================
 // Parameter Variables
@@ -28,38 +34,63 @@
     var substrEnd = document.currentScript.src.indexOf('.js');
     var scriptName = document.currentScript.src.substring(substrBegin + 1, substrEnd);
     var parameters = PluginManager.parameters(scriptName);
-    //credit to Traverse of RPG Maker Central Forums for the above scriplet via SoulPour777 RPG Maker MV scripting tutorial video
-    //====================================================================================================================
-    // Overwritten function: expTotal
-    //====================================================================================================================
-    // Change how Exp is calculated
-    // #Boomy
-    //====================================================================================================================
-    var _SRPG_Game_Troop_expTotal = Game_Troop.prototype.expTotal;
-    Game_Troop.prototype.expTotal = function () {
-        if ($gameSystem.isSRPGMode() == true) {
-            var actor = $gameParty.battleMembers()[0];
-            if (this.SrpgBattleEnemys() && this.SrpgBattleEnemys().length > 0) {
-                if (this.isAllDead()) {
-                    var exp = this.deadMembers().reduce(function (r, enemy) {
-                        return r + eval(parameters['Kill EXP Formula']);
-                    }, 0);
-                    return Math.floor(exp);
-                } else {
-                    var exp = 0;
-                    for (var i = 0; i < this.members().length; i++) {
-                        var enemy = this.members()[i];
-                        exp += eval(parameters['Regular EXP Formula']);
+    BattleManager.makeRewards = function () {
+        this._rewards = {};
+        this._rewards.gold = $gameTroop.goldTotal();
+        var defaultExp = $gameTroop.expTotal();
+        if ($gameSystem.isSRPGMode()) {
+            //User is an actor 
+            if ($gameSystem.EventToUnit($gameTemp.activeEvent().eventId())[0] == "actor") {
+                //If target is enemy
+                if ($gameSystem.EventToUnit($gameTemp.targetEvent().eventId())[0] == "enemy") {
+                    //Reduce exp gain if average parameter is Set
+                    if (eval(parameters['Average AoE Target EXP'])) {
+                        defaultExp = defaultExp / ($gameTemp._areaTargets.length + 1);
                     }
-                    return Math.floor(exp);
+                    //Apply multiplier to EXP if EXP multiplier is set 
+                    if (parameters['EXP multiplier'] !== 1) {
+                        //Work out targetsAverageLevel, targetsMaxLevel and targetsMinLevel
+                        var levelArray = [Number($gameSystem.EventToUnit($gameTemp.targetEvent().eventId())[1].level !== undefined ? $gameSystem.EventToUnit($gameTemp.targetEvent().eventId())[1].level : $gameSystem.EventToUnit($gameTemp.targetEvent().eventId())[1].enemy().meta.srpgLevel)];
+                        for (var i = 0; i < $gameTemp._areaTargets.length; i++) {
+                            console.log($gameSystem.EventToUnit($gameTemp._areaTargets[i].event.eventId())[1].enemy().meta.srpgLevel);
+                            var level = Number($gameSystem.EventToUnit($gameTemp._areaTargets[i].event.eventId())[1].level !== undefined ? $gameSystem.EventToUnit($gameTemp._areaTargets[i].event.eventId())[1].level : $gameSystem.EventToUnit($gameTemp._areaTargets[i].event.eventId())[1].enemy().meta.srpgLevel);
+                            levelArray.push(level);
+                        }
+                        var average = levelArray = > levelArray.reduce((p, c) = > p + c, 0) / levelArray.length;
+                        var targetsAverageLevel = average(levelArray);
+                        var targetsMaxLevel = Math.max(...levelArray);
+                        var targetsMinLevel = Math.min(...levelArray);
+                        var user = $gameSystem.EventToUnit($gameTemp.activeEvent().eventId())[1];
+                        defaultExp *= eval(parameters['EXP multiplier']);
+                    }
                 }
-            } else {
-                var exp = eval(parameters['Skill EXP Formula']);
-                return Math.floor(exp);
             }
+            //User is an enemy 
+            if ($gameSystem.EventToUnit($gameTemp.activeEvent().eventId())[0] == "enemy") {
+                //Using AoEAnimation.js, exp is averaged when multiple actors are targetted
+                //Apply multiplier to EXP if EXP multiplier is set 
+                if (parameters['EXP multiplier'] !== 1) {
+                    //Work out targetsAverageLevel, targetsMaxLevel and targetsMinLevel
+                    var levelArray = [Number($gameSystem.EventToUnit($gameTemp.targetEvent().eventId())[1].level !== undefined ? $gameSystem.EventToUnit($gameTemp.targetEvent().eventId())[1].level : $gameSystem.EventToUnit($gameTemp.targetEvent().eventId())[1].enemy().meta.srpgLevel)];
+                    for (var i = 0; i < $gameTemp._areaTargets.length; i++) {
+                        var level = Number($gameSystem.EventToUnit($gameTemp._areaTargets[i].event.eventId())[1].level !== undefined ? $gameSystem.EventToUnit($gameTemp._areaTargets[i].event.eventId())[1].level : $gameSystem.EventToUnit($gameTemp._areaTargets[i].event.eventId())[1].enemy().meta.srpgLevel);
+                        levelArray.push(level);
+                    }
+                    var average = levelArray = > levelArray.reduce((p, c) = > p + c, 0) / levelArray.length;
+                    var targetsAverageLevel = average(levelArray);
+                    var targetsMaxLevel = Math.max(...levelArray);
+                    var targetsMinLevel = Math.min(...levelArray);
+                    var user = $gameSystem.EventToUnit($gameTemp.activeEvent().eventId())[1];
+                    if (user.level == undefined) {
+                        user.level = user.enemy().meta.srpgLevel;
+                    }
+                    defaultExp *= 1 / eval(parameters['EXP multiplier']);
+                }
+            }
+            this._rewards.exp = defaultExp;
         } else {
-            return _SRPG_Game_Troop_expTotal.call(this);
+            this._rewards.exp = $gameTroop.expTotal();
         }
+        this._rewards.items = $gameTroop.makeDropItems();
     };
-/*
-})();*/
+})();
